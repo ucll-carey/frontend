e3682f6afb2c79cbd85156e6fce7a61e65457c3a
Larry Boltovskoi <larry.bolt@gmail.com>
2018-11-11 21:27:40 +0100

    List games on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+<b-container>
+    <h2>Games</h2>
+    <b-card v-for="(game, index) in games" :key="index" :title="game.name"
+          style="max-width: 20rem;"
+          :footer="game.city"
+          class="mb-2">
+    <p class="card-text">{{game.description}}</p>
+    <b-button :href="`/game/${game.id}`" variant="primary">Start This game</b-button>
+  </b-card>
+</b-container>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            loading: false,
+            games: [],
+        }
+    },
+    mounted() {
+        axios
+            .get('http://localhost:8080/api/game/')
+            .then(response => {
+                this.games = response.data
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
+    }
+}
+</script>
